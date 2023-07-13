35337f1016d70a0ac72bc9caa5551c0a2d1e86f8
Steve De Jongh <de.jongh.steve@gmail.com>
2023-07-13 15:08:23 -0700

    Adding customers

--- app.rb
+++ app.rb
@@ -24,6 +24,14 @@ after do
   @storage.disconnect
 end
 
+def input_name_validation(name)
+  if name.nil?
+    "Please enter a valid name."
+  elsif !(1..100).cover?(name.length)
+    "Name must be less that 100 characters."
+  end
+end
+
 get '/' do
   erb :home, layout: :layout
 end
@@ -34,6 +42,19 @@ end
 
 get '/customer/new' do
   erb :new_customer, layout: :layout
+end
+
+post '/customer/new' do
+  @name = params[:customer_name]
+  error = input_name_validation(@name)
+  if error
+    session[:error] = error
+    redirect '/customer/new'
+  else
+    @storage.add_customer(@name)
+    session[:success] = "Customer #{@name} has been created."
+    redirect '/'
+  end  
 end
 
 get '/item/new' do
--- database.rb
+++ database.rb
@@ -1,6 +1,7 @@
 # POS database
 
 require 'pg'
+require 'pry'
 
 class Database
   def initialize
@@ -9,6 +10,18 @@ class Database
 
   def disconnect
     @db.close
+  end
+
+  def query(sql, *params)
+    @db.exec_params(sql, params)
+  end
+
+  def add_customer(name)
+    sql = <<~SQL
+      INSERT INTO customer (name)
+      VALUES ($1);
+    SQL
+    query(sql, name)
   end
 
 end
